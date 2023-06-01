8ab125dd479767e85a479ed9e149a22d290354f8
Fzzffz <1214397309@qq.com>
2023-06-02 00:13:35 +0800

    写了UseEntityMapper的增删改查

--- src/main/java/com/jdsbbmq/wjxbx/dao/UserEntityMapper.java
+++ src/main/java/com/jdsbbmq/wjxbx/dao/UserEntityMapper.java
@@ -5,10 +5,17 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Mapper
 @Repository
 public interface UserEntityMapper {
 
     // 登录
     UserEntity login(@Param("username") String username, @Param("password") String password);
+    List<UserEntity> selectAll();
+    UserEntity selectUser(String id);
+    int insertUser(UserEntity userEntity);
+    int updateUser(UserEntity userEntity);
+    int deleteUser(String id);
 }
