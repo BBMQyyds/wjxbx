5cbd019b0ae6300cf3a039b79361d592f95ab732
Fzzffz <1214397309@qq.com>
2023-06-02 17:17:33 +0800

    增加了Druid和Index.html Druid登录为admin和123456 前缀加上druid就可以切换到druid界面

--- src/main/java/com/jdsbbmq/wjxbx/controller/TestController.java
+++ src/main/java/com/jdsbbmq/wjxbx/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.jdsbbmq.wjxbx.controller;
 
+import com.jdsbbmq.wjxbx.dao.UserEntityMapper;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    @Resource
+    UserEntityMapper userEntityMapper;
     @RequestMapping("/test")
-    public String test() {
+    public String test(@RequestBody String username) {
+        userEntityMapper.selectIdByUsername(username);
         return "test";
     }
 }
