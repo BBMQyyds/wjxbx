c4f211abdac8c173ffc74ed783a1719f12ce9b28
33698 <3369855428@qq.com>
2023-06-06 09:12:25 +0800

    sonarqube+github测试2

--- src/main/java/com/jdsbbmq/wjxbx/controller/TestController.java
+++ src/main/java/com/jdsbbmq/wjxbx/controller/TestController.java
@@ -1,8 +1,5 @@
 package com.jdsbbmq.wjxbx.controller;
 
-import com.jdsbbmq.wjxbx.dao.UserEntityMapper;
-import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
