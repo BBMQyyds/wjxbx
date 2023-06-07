887b7eab92e5bfbf0bff897216504216844c5f49
Fzzffz <1214397309@qq.com>
2023-06-07 15:15:28 +0800

    增加了Springboot3整合Swagger3，资料来自：https://zhuanlan.zhihu.com/p/610358206

--- src/main/java/com/jdsbbmq/wjxbx/WjxbxApplication.java
+++ src/main/java/com/jdsbbmq/wjxbx/WjxbxApplication.java
@@ -2,13 +2,10 @@ package com.jdsbbmq.wjxbx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class WjxbxApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WjxbxApplication.class, args);
         System.out.println("Your application is running here: http://localhost:8080\n");
     }
-
 }
--- src/main/java/com/jdsbbmq/wjxbx/config/SwaggerConfig.java
+++ src/main/java/com/jdsbbmq/wjxbx/config/SwaggerConfig.java
@@ -1,22 +1,16 @@
-//package com.jdsbbmq.wjxbx.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration //配置类
-//@EnableSwagger2// 开启Swagger2的自动配置
-//public class SwaggerConfig {
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.jdsbbmq.wjxbx.controller"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//}
+package com.jdsbbmq.wjxbx.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration //配置类
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI().info(new Info() //
+                .title("SpringDoc API Test") //
+                .description("SpringDoc Simple Application Test") //
+                .version("0.0.1"));
+    }
+}
