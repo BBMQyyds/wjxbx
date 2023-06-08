d316d3ce1f832fd2288f2dc874cedfe732fb32e3
Fzzffz <1214397309@qq.com>
2023-06-08 13:43:36 +0800

    增加了swagger的一些注释，用来标注实体类和接口名

--- src/main/java/com/jdsbbmq/wjxbx/controller/UserController.java
+++ src/main/java/com/jdsbbmq/wjxbx/controller/UserController.java
@@ -3,12 +3,12 @@ package com.jdsbbmq.wjxbx.controller;
 import com.jdsbbmq.wjxbx.bean.user.User;
 import com.jdsbbmq.wjxbx.dao.entity.UserEntity;
 import com.jdsbbmq.wjxbx.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +24,7 @@ public class UserController {
      */
 
     // 登录
+    @Operation(summary = "登录接口", description = "输入username与password进行登录")
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public User login(@RequestBody String username, @RequestBody String password) {
         UserEntity userEntity = userService.login(username, password);
@@ -31,7 +32,15 @@ public class UserController {
     }
 
     // 根据id查询用户
+
+
     @RequestMapping(value = "/selectUserById", method = RequestMethod.POST)
+    @Operation(summary = "根据id查询用户", description = "根据id进行查询")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "成功获取用户信息"),
+            @ApiResponse(responseCode = "404", description = "未找到对应的用户信息")
+    })
+    @ResponseBody
     public User selectUserById(@RequestBody String id) {
         UserEntity userEntity = userService.selectUserById(id);
         return userEntity.toUser();
