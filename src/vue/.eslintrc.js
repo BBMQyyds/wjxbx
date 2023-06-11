c9d0fd99368afcda1ee2eaad4a6a4ccc116290e7
33698 <3369855428@qq.com>
2023-06-11 13:25:43 +0800

    vue大改动

---
+++ src/vue/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        //在rules中添加自定义规则
+        //关闭组件命名规则
+        "vue/multi-word-component-names": "off",
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
+
