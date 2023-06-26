137f49ca61e488b548d8fbad489e05d84c97b9b7
Fzzffz <1214397309@qq.com>
2023-06-26 14:44:37 +0800

    Revert "完善接口"
    This reverts commit 308dcdccf4e7ef9a519ab31742bd4ef76e974f50.

--- src/main/java/com/jdsbbmq/wjxbx/controller/AnswerController.java
+++
@@ -1,9 +0,0 @@
-package com.jdsbbmq.wjxbx.controller;
-
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AnswerController {
-
-}
--- src/main/java/com/jdsbbmq/wjxbx/service/Impl/AnswerServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.jdsbbmq.wjxbx.service.Impl;
-
-import com.jdsbbmq.wjxbx.service.AnswerService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AnswerServiceImpl implements AnswerService{
-
-}
